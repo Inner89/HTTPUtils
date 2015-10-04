cd81835a075263d3ff6d414dc6ce6ee7275d28e7
SpongyBacon <spongybacon@hotmail.co.uk>
2015-10-04 01:11:42 +0100

    Add comments

--- src/pw/sponges/httputils/GET.java
+++ src/pw/sponges/httputils/GET.java
@@ -17,24 +17,44 @@ public class GET {
 
     private GETResult result = null;
 
+    /**
+     * No args constructor so it can be used as a builder
+     */
     public GET() {
         this.ssl = false;
         this.url = null;
         this.properties = new HashMap<>();
     }
 
-    public GET(boolean ssl, String url, String body) {
+    /**
+     * Constructor without header properties map.
+     * Header properties should be added with #addProperty
+     * @param ssl - use https?
+     * @param url - the url to make the request to
+     */
+    public GET(boolean ssl, String url) {
         this.ssl = ssl;
         this.url = url;
         this.properties = new HashMap<>();
     }
 
-    public GET(boolean ssl, String url, Map<String, String> properties, String body) {
+    /**
+     * Constructor with all params set.
+     * @param ssl - use https?
+     * @param url - the url to make the request to
+     * @param properties - the header properties map
+     */
+    public GET(boolean ssl, String url, Map<String, String> properties) {
         this.ssl = ssl;
         this.url = url;
         this.properties = properties;
     }
 
+    /**
+     * Creates the connection & gets the results.
+     * @return this - builder pattern
+     * @throws IOException - thrown by URL connections
+     */
     public GET connect() throws IOException {
         URL url = new URL(this.url);
 
@@ -65,37 +85,74 @@ public class GET {
         return this;
     }
 
+    /**
+     * Returns the results from the GET request in a GETResult object.
+     * @return result - the GETResult object
+     */
     public GETResult getResult() {
         return result;
     }
 
+    /**
+     * Using https?
+     * @return ssl
+     */
     public boolean isSsl() {
         return ssl;
     }
 
+    /**
+     * Should https be used instead of http?
+     * @param ssl - boolean for using https or http
+     * @return this - builder pattern
+     */
     public GET setSsl(boolean ssl) {
         this.ssl = ssl;
         return this;
     }
 
+    /**
+     * The String url that is being sent the request.
+     * @return url
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Sets the url that the request is being sent to.
+     * @param url
+     * @return this - builder pattern
+     */
     public GET setUrl(String url) {
         this.url = url;
         return this;
     }
 
+    /**
+     * The headers for the request in a Map.
+     * @return properties - the headers Map
+     */
     public Map<String, String> getProperties() {
         return properties;
     }
 
+    /**
+     * Sets the headers Map for the request.
+     * @param properties - the headers Map
+     * @return this - builder pattern
+     */
     public GET setProperties(Map<String, String> properties) {
         this.properties = properties;
         return this;
     }
 
+    /**
+     * Adds a header for the request to the properties Map.
+     * @param key - the header name
+     * @param value - the header value
+     * @return this - builder pattern
+     */
     public GET addProperty(String key, String value) {
         properties.put(key, value);
         return this;
@@ -108,24 +165,48 @@ class GETResult {
     private final int code;
     private final String content;
 
+    /**
+     * Constructor with only the response code.
+     * Would be used if no content was returned from the request.
+     * @param code - the request response code
+     */
     public GETResult(int code) {
         this.code = code;
         this.content = null;
     }
 
+    /**
+     * Constructor with the response code and the content from the request.
+     * @param code - the request response code
+     * @param content - the returned content from the request
+     */
     public GETResult(int code, String content) {
         this.code = code;
         this.content = content;
     }
 
+    /**
+     * Returns the response code for the request.
+     * @return code - the response code
+     */
     public int getCode() {
         return code;
     }
 
+    /**
+     * Returns the content for the request.
+     * @return content - the String content
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * Boolean to check if there is content in the results.
+     * Would be used to see if any content was returned from the request,
+     * e.g. if there was an error in the request.
+     * @return boolean - if there is content in the results
+     */
     public boolean hasContent() {
         return content != null;
     }
--- src/pw/sponges/httputils/POST.java
+++ src/pw/sponges/httputils/POST.java
@@ -19,6 +19,9 @@ public class POST {
 
     private POSTResult result = null;
 
+    /**
+     * No args constructor so it can be used as a builder
+     */
     public POST() {
         this.ssl = false;
         this.url = null;
@@ -26,6 +29,14 @@ public class POST {
         this.body = null;
     }
 
+
+    /**
+     * Constructor without header properties map.
+     * Header properties should be added with #addProperty
+     * @param ssl - use https?
+     * @param url - the url to make the request to
+     * @param body - the body to send in the request
+     */
     public POST(boolean ssl, String url, String body) {
         this.ssl = ssl;
         this.url = url;
@@ -33,6 +44,13 @@ public class POST {
         this.properties = new HashMap<>();
     }
 
+    /**
+     * Constructor with all params set.
+     * @param ssl - use https?
+     * @param url - the url to make the request to
+     * @param properties - the header properties map
+     * @param body - the body to send in the request
+     */
     public POST(boolean ssl, String url, Map<String, String> properties, String body) {
         this.ssl = ssl;
         this.url = url;
@@ -40,6 +58,11 @@ public class POST {
         this.body = body;
     }
 
+    /**
+     * Creates the connection & gets the results.
+     * @return this - builder pattern
+     * @throws IOException - thrown by URL connections
+     */
     public POST connect() throws IOException {
         URL url = new URL(this.url);
 
@@ -75,46 +98,92 @@ public class POST {
         return this;
     }
 
+    /**
+     * Returns the results from the POST request in a POSTResult object.
+     * @return result - the POSTResult object
+     */
     public POSTResult getResult() {
         return result;
     }
 
+    /**
+     * Using https?
+     * @return ssl
+     */
     public boolean isSsl() {
         return ssl;
     }
 
+    /**
+     * Should https be used instead of http?
+     * @param ssl - boolean for using https or http
+     * @return this - builder pattern
+     */
     public POST setSsl(boolean ssl) {
         this.ssl = ssl;
         return this;
     }
 
+    /**
+     * The String url that is being sent the request.
+     * @return url
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Sets the url that the request is being sent to.
+     * @param url
+     * @return this - builder pattern
+     */
     public POST setUrl(String url) {
         this.url = url;
         return this;
     }
 
+    /**
+     * The headers for the request in a Map.
+     * @return properties - the headers Map
+     */
     public Map<String, String> getProperties() {
         return properties;
     }
 
+    /**
+     * Sets the headers Map for the request.
+     * @param properties - the headers Map
+     * @return this - builder pattern
+     */
     public POST setProperties(Map<String, String> properties) {
         this.properties = properties;
         return this;
     }
 
+    /**
+     * Adds a header for the request to the properties Map.
+     * @param key - the header name
+     * @param value - the header value
+     * @return this - builder pattern
+     */
     public POST addProperty(String key, String value) {
         properties.put(key, value);
         return this;
     }
 
+    /**
+     * Gets the body of the request.
+     * @return body - the request body
+     */
     public String getBody() {
         return body;
     }
 
+    /**
+     * Sets the body of the request.
+     * @param body - the request body
+     * @return this - builder pattern
+     */
     public POST setBody(String body) {
         this.body = body;
         return this;
